78a2e9d0111ddb1fec4eddc85ab51a55dbc52aa5
junsu <melon8372@gmail.com>
2020-03-17 00:06:55 +0900

    Initial commit

---
+++ src/main/java/com/junsu/webservice/junsuspringboot2webservicedemo/JunsuSpringboot2WebserviceDemoApplication.java
@@ -0,0 +1,13 @@
+package com.junsu.webservice.junsuspringboot2webservicedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunsuSpringboot2WebserviceDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JunsuSpringboot2WebserviceDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/junsu/webservice/junsuspringboot2webservicedemo/JunsuSpringboot2WebserviceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junsu.webservice.junsuspringboot2webservicedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunsuSpringboot2WebserviceDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
