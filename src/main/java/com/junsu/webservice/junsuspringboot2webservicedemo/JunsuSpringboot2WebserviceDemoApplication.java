75d6f42ac1fdc28e49c97639a12f4bb62818b923
junsu <melon8372@gmail.com>
2020-03-19 03:13:45 +0900

    feature: Add Post CRUD domain

--- src/main/java/com/junsu/webservice/junsuspringboot2webservicedemo/JunsuSpringboot2WebserviceDemoApplication.java
+++ src/main/java/com/junsu/webservice/junsuspringboot2webservicedemo/JunsuSpringboot2WebserviceDemoApplication.java
@@ -2,7 +2,9 @@ package com.junsu.webservice.junsuspringboot2webservicedemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class JunsuSpringboot2WebserviceDemoApplication {
 
---
+++ src/main/java/com/junsu/webservice/junsuspringboot2webservicedemo/boundaries/controller/PostsApiController.java
@@ -0,0 +1,30 @@
+package com.junsu.webservice.junsuspringboot2webservicedemo.boundaries.controller;
+
+import com.junsu.webservice.junsuspringboot2webservicedemo.boundaries.dto.PostsResponseDto;
+import com.junsu.webservice.junsuspringboot2webservicedemo.boundaries.dto.PostsSaveRequestDto;
+import com.junsu.webservice.junsuspringboot2webservicedemo.boundaries.dto.PostsUpdateRequestDto;
+import com.junsu.webservice.junsuspringboot2webservicedemo.service.PostsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long create(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+}
