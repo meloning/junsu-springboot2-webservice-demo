fcdf566c8e394d6823e29c8f30dae2e82c84bc42
junsu <melon8372@gmail.com>
2020-03-23 18:29:26 +0900

    comment: Add hasRole content

--- src/main/java/com/junsu/webservice/junsuspringboot2webservicedemo/config/auth/SecurityConfig.java
+++ src/main/java/com/junsu/webservice/junsuspringboot2webservicedemo/config/auth/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                     .authorizeRequests()
                     .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
-                    .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                    .antMatchers("/api/v1/**").hasRole(Role.USER.name()) // prefix: ROLE_
                     .anyRequest().authenticated()
                 .and()
                     .logout()
