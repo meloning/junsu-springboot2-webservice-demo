267249940af128903e81db684daf80bf2388383d
junsu <melon8372@gmail.com>
2020-03-23 16:43:20 +0900

    feature: Add SecurityConfig
    SecurityConfig 설정을 안했기에 logout후 index root 페이지로 리다이렉션하지 않음.
    또한, index page 접근시 google login url로 리다이렉션하며,
    /h2-console 403 권한 에러가 발생
    
    SecurityConfig로 설정을 등록해야함.

---
+++ src/main/java/com/junsu/webservice/junsuspringboot2webservicedemo/config/auth/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.junsu.webservice.junsuspringboot2webservicedemo.config.auth;
+
+import com.junsu.webservice.junsuspringboot2webservicedemo.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                // h2-console 화면을 사용하기 위해 disable
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                    .authorizeRequests()
+                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
+                    .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                    .anyRequest().authenticated()
+                .and()
+                    .logout()
+                        .logoutSuccessUrl("/")
+                .and()
+                    .oauth2Login()
+                        .userInfoEndpoint()
+                            .userService(customOAuth2UserService);
+    }
+}
