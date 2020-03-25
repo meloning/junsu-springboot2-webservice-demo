3dac74d256650a8946a0fbbab61a6f033ac47737
junsu <melon8372@gmail.com>
2020-03-26 04:09:14 +0900

    build-bugfix: Edit nohup with config.location(oauth) in deploy.sh
    Security config 설정이 없어서 bean error 발생

--- script/deploy.sh
+++ script/deploy.sh
@@ -45,4 +45,8 @@ JAR_NAME=$(ls -tr $REPOSITORY/ | grep *.jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
 
-nohup java -jar $REPOSITORY/$JAR_NAME 2>&1 &
+# nohup java -jar $REPOSITORY/$JAR_NAME 2>&1 &
+
+nohup java -jar \
+-Dspring.config.location=classpath:/application.yml,/home/ec2-user/app/application-oauth.yml \
+$REPOSITORY/$JAR_NAME 2>&1 &
